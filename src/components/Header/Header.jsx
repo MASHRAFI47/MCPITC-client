342dad4320ff5eb9b961ab731db0963a486ff3b4
Mashrafi Bin Nur <mbngms@gmail.com>
2026-03-28 13:45:37 +0600

    hamburger changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -79,7 +79,7 @@ const Header = () => {
         <div className={`${theme === "" ? "bg-[#f4f6f8]" : ""} border-b-8 border-[#d8dbe5]`}>
             <div className="navbar container mx-auto">
                 <div className="navbar-start">
-                    <div className="dropdown text-white z-[1000]">
+                    <div className={`dropdown ${theme == "synthwave" ? "text-white" : "text-black"} z-[1000]`}>
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
